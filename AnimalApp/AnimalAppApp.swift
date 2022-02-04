c5d7f443d81b597794eef8eb6f821744a1f892f4
amirreza jolani <amirreza.jolani@yahoo.com>
2022-02-04 12:06:47 +0330

    Initial Commit

---
+++ AnimalApp/AnimalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalAppApp.swift
+//  AnimalApp
+//
+//  Created by joooli on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
